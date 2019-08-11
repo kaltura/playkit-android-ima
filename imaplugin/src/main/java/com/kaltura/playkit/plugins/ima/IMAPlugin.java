e17c2c5e19f757321ff3d13716f5efedaa95b3d1
Gilad Nadav <gilad.nadav@kaltura.com>
2019-08-11 13:31:28 +0300

    Fix FEM-2612

--- imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
+++ imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
@@ -496,8 +496,9 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
 
     @Override
     protected void onDestroy() {
-        log.d("IMA Start onDestroy");
-        boolean adManagerInitialized = (adsManager != null); // FEM-2600
+        log.d("IMA Start onDestroy lastAdEventReceived = " + lastAdEventReceived);
+        boolean adManagerInitialized = (adsManager != null || lastAdEventReceived != null); // FEM-2600
+
         destroyIMA();
         if (adDisplayContainer != null && adManagerInitialized) {
             adDisplayContainer.destroy();
