0143132ea5ed5599341ed21231a6ae2d269ebd7f
Gilad Nadav <gilad.nadav@kaltura.com>
2019-11-24 17:56:34 +0200

    Update IMAPlugin.java

--- imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
+++ imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
@@ -1165,8 +1165,7 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
                 if (checkIfDiscardAdRequired()) {
                     for (Long cuePoint : adTagCuePoints.getAdCuePoints()) {
                         if (cuePoint != 0 && cuePoint != -1 && ((cuePoint / Consts.MILLISECONDS_MULTIPLIER_FLOAT) < playbackStartPosition)) {
-                            log.d("discardAdBreak");
-                            // Discards current ad break and resumes content. If there is no current ad then the next ad break is discarded.
+                            log.d("discardAdBreak"); // Discards current ad break and resumes content. If there is no current ad then the next ad break is discarded.
                             adsManager.discardAdBreak();
                             playbackStartPosition = null; // making sure it will nu be done again.
                             break;
