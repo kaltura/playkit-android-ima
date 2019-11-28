71bb971992fc11186c7118c0ae2d7fbeba07c749
Gourav Saxena <gourav.saxena@kaltura.com>
2019-11-28 13:44:42 +0530

    set it to true in case LOG set it to false

--- imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
+++ imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
@@ -1159,6 +1159,7 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
                 }
 
                 isAdDisplayed = true;
+                isAdRequested = true;
                 messageBus.post(new AdEvent.AdStartedEvent(adInfo));
                 if (adsManager != null && appIsInBackground) {
                     log.d("AD STARTED and pause");
