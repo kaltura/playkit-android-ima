1f3319dddf63bc97f702c59f2cf813fe71cff32f
Gourav Saxena <gourav.saxena@kaltura.com>
2020-11-11 12:31:09 +0530

    - Added new params in AdInfo

--- imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
+++ imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
@@ -924,6 +924,12 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
         String contentType = ad.getContentType();
         String adId = ad.getAdId();
         String adSystem = ad.getAdSystem();
+        String creativeId = ad.getCreativeId();
+        String creativeAdId = ad.getCreativeAdId();
+        String advertiserName = ad.getAdvertiserName();
+        String dealId = ad.getDealId();
+        String surveyUrl = ad.getSurveyUrl() == null ? "" : ad.getSurveyUrl();
+        String traffickingParams = ad.getTraffickingParameters();
         int adHeight = ad.isLinear() ? ad.getVastMediaHeight() : ad.getHeight();
         int adWidth  = ad.isLinear() ? ad.getVastMediaWidth() : ad.getWidth();
         int mediaBitrate = ad.getVastMediaBitrate() != 0 ? ad.getVastMediaBitrate() * KB_MULTIPLIER : -1;
@@ -946,8 +952,9 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
 
         AdInfo adInfo = new AdInfo(adDescription, adDuration, adPlayHead,
                 adTitle, isAdSkippable, skipTimeOffset,
-                contentType, adId,
-                adSystem,
+                contentType, adId, adSystem,
+                creativeId, creativeAdId, advertiserName,
+                dealId, surveyUrl, traffickingParams,
                 adHeight,
                 adWidth,
                 mediaBitrate,
