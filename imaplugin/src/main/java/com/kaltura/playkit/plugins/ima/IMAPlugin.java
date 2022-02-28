6db72d570fd86aa817cd05dd8689b1519979dc4e
Gourav Saxena <gourav.saxena@kaltura.com>
2022-02-28 18:41:17 +0530

    - Travis build

--- imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
+++ imaplugin/src/main/java/com/kaltura/playkit/plugins/ima/IMAPlugin.java
@@ -966,6 +966,7 @@ public class IMAPlugin extends PKPlugin implements AdsProvider, com.google.ads.i
         //log.d("getDuration: " + duration);
         return duration;
     }
+    
 
     @Override
     public long getCurrentPosition() {
